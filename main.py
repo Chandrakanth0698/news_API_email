31831dcbc9c1043c191fef79c85b1d2cf9852bdb
Chandrakanth0698 <chandrakanth0698@gmail.com>
2023-11-04 14:51:58 -0400

    Limit news, Add url and fix bugs

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import requests
+from send_email import send_email
 # get api key and API url to get access to data
+# you can change the category or country with f- strings
 API_KEY = "b997584a6f7c46e7b049f0774987178a"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-10-03&" \
-      "sortBy=publishedAt&apiKey=b997584a6f7c46e7b049f0774987178a"
-# get the request object using get function
+url = "https://newsapi.org/v2/top-headlines?" \
+      "country=us" \
+      "&category=business" \
+      "&apiKey=b997584a6f7c46e7b049f0774987178a"
+
+# Make request
 request = requests.get(url)
-# get data in the dict using .json() function
+
+# Get a dictionary with data
 content = request.json()
 
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+# Access the article titles and description
+body = "Subject: Today's Business News from Chandu's python API script " + "\n"
+for article in content["articles"][:20]:
+    if article["title"] and article["description"] :
+        body = body + "Title:" + article["title"] + "\n" + "Description: " + article["description"] + "\n" \
+               + article["url"] + "\n" + "Content:" + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
 
---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "de.chandra226@gmail.com"
+    password = "mcapescgbhcoxtgr"
+    receiver = "de.chandra226@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
