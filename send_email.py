81fbaf6594438ca6e1405f1b49525c8402f00ece
Chandrakanth0698 <111915753+Chandrakanth0698@users.noreply.github.com>
2023-11-04 15:11:49 -0400

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "de.chandra226@gmail.com"
-    password = "mcapescgbhcoxtgr"
+    password = "YOUR_GMAIL_SMTP_PASSWORD"
     receiver = "de.chandra226@gmail.com"
     context = ssl.create_default_context()
 
